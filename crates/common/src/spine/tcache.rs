e87bff5b07aad426bb2997b25c864f345cea7019
vladimir-ea <vladimir@gattaca.com>
2026-09-15 10:11:34 +0100

    rebase snafu

--- crates/common/src/spine/tcache.rs
+++ crates/common/src/spine/tcache.rs
@@ -16,7 +16,6 @@ pub use consumer::{
     AcquiredRange, AcquiredRead, AcquiredWithOffset, Consumer, RandomAccessConsumer, TCacheRead,
 };
 use flux::{Timer, timing::Nanos, tracing};
-
 pub use producer::{MultiProducer, Producer, Reservation, TCacheProducer};
 pub use sub_reservation::{
     AcquiredSubReservation, PendingSubReservation, SubLayout, SubReservation, SubReservationError,
@@ -47,7 +46,6 @@ const fn lag_threshold(len: u32) -> u64 {
 
 mod cache_frame;
 mod consumer;
-mod gossip_frame;
 mod metrics;
 mod producer;
 mod sub_reservation;
